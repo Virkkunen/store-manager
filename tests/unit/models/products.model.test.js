247ec5cb9c606d46f81a920a76c1e8842126a7bd
Virkkunen <vrkknn@vrkknn.net>
2023-03-22 19:10:16 -0300

    testes

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -22,6 +22,8 @@ describe('Testa /products models', () => {
   afterEach(() => sinon.restore());
 
   describe('Função getAllProducts', () => {
+    afterEach(() => sinon.restore());
+
     it('Retorna todos os produtos', async () => {
       sinon.stub(connection, 'execute').resolves([getAllProductsReturn]);
       const result = await productsModel.getAllProducts();
@@ -30,6 +32,8 @@ describe('Testa /products models', () => {
   });
 
   describe('Função getProductById', () => {
+    afterEach(() => sinon.restore());
+
     it('Retorna o produto correto', async () => {
       sinon.stub(connection, 'execute').resolves([getProductByIdReturn]);
       const result = await productsModel.getProductById(1);
@@ -37,18 +41,39 @@ describe('Testa /products models', () => {
     });
 
     xit('Retorna erro 404 quando o produto não existe', async () => {
-      // sinon.stub(connection, 'execute').resolves();
-      // const result = await productsModel.getProductById(10);
-      // expect(result).to.deep.equal(getProductByIdError);
+      sinon.stub(connection, 'execute').resolves();
+      const result = await productsModel.getProductById(10);
+      expect(result).to.deep.equal(getProductByIdError);
     });
   });
 
   describe('Função createProduct', () => {
+    afterEach(() => sinon.restore());
+
     it('Cria o produto corretamente', async () => {
       sinon.stub(connection, 'execute').resolves([insertIdProductReturn]);
       const result = await productsModel.createProduct(createProductMock);
       // eu nem sei mais o porque dos []
       expect([result]).to.deep.equal(insertIdProductReturn);
+    });
+  });
+
+  describe('Testes gerais de product', () => {
+    afterEach(() => sinon.restore());
+
+    it('Atualiza o produto', async () => {
+      const expected = [{ id: 2, name: 'Update' }];
+      sinon.stub(connection, 'execute').resolves([expected]);
+      const result = await productsModel.updateProduct(2, 'Update');
+      expect([result]).to.deep.equal(expected);
+    });
+
+    it('Apaga o produto', async () => {
+      const expected = 1;
+      sinon.stub(connection, 'execute').resolves([expected]);
+      const result = await productsModel.deleteProduct(1);
+      console.log(result)
+      expect(result).to.equal(expected);
     });
   });
 });
